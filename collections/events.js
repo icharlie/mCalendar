cfc07d6984d6ab23807cc27d983ee6e4697d8f1b
Charlie Chang <icharlie.osu@gmail.com>
2015-05-23 13:24:58 -0700

    Move collections to collections folder

--- collections/events.js
+++ collections/events.js
@@ -1,13 +1,13 @@
-Events = new Meteor.Collection('events');
+Events = new Mongo.Collection('events');
 Events.allow({
-  insert: function (userId, event) {
-    return true;
+  insert: function(userId, event) {
+    return true
   },
-  update: function (userId, event) {
+  update: function(userId, event) {
     var email = Meteor.users.find({_id: userId}).fetch()[0].emails[0].address;
     return event.ownerId === userId || event.partnerIds.indexOf(userId) !== -1 || event.pendingEmail.indexOf(email) !== -1;
   },
-  remove: function (userId, event) {
+  remove: function(userId, event) {
     return event.ownerId === userId;
   }
 });
