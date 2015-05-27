24270d9520d61c7f7159a46dd36615b9e7fbe212
Charlie <icharlie.osu@gmail.com>
2015-05-27 00:28:34 -0700

    API events.

--- server/route.js
+++ server/route.js
@@ -16,25 +16,7 @@ Router.onBeforeAction(
   }
 );
 
-Router.route('api/events',
-  function() {
-    if (!this.user) {
-      return {isLoggedIn: false};
-    }
-
-    return {
-      isLoggedIn: true,
-      events: Events.find({$or: [{partnerIds: this.user._id}, {ownerId: this.user._id}]}).fetch()
-    };
-  },
-
-  {
-    where: 'server'
-  }
-);
-
 var user = Router.route('/user/:userId', {where: 'server'});
-
 user.get(function() {
   return this.response.end(
     JSON.stringify(
@@ -67,50 +49,6 @@ user.put(function() {
     this.response.statusCode = 404;
     return this.response.end(JSON.stringify({msg: 'update failed. Can\'t find matched user'}));
   }
-});
-
-var event = Router.route('/event/create')
-event.post(function() {
-  if (!this.user) {
-    return {isLoggedIn: false};
-  }
-
-  if (checkRequireField(this.params)) {
-    // TODO: HTTP status code and more description.
-    return {
-      isLoggedIn: true,
-      err: 'Please provide enough event information. We need title, desc, start, end, allDay',
-      data: this.params
-    };
-  }
-
-  var evt = {};
-  var keys = ['title', 'desc', 'start', 'end', 'date', 'allDay', 'ownerId', 'address', 'lat', 'lng'];
-
-  keys.forEach(function(key) {
-    evt[key] = this.params[key];
-  });
-
-  if (evt.allDay) {
-    evt.allDay = EJSON.parse(allDay);
-  } else {
-    evt.allDay = false;
-  }
-
-  if (allDay) {
-    evt.start = moment(evt.date).toDate();
-    evt.end = moment(evt.date).toDate();
-  } else {
-    evt.start = moment(evt.start).toDate();
-    evt.end = moment(evt.start).toDate();
-  }
-
-  var eventId = Events.insert(evt);
-
-  return {
-    isLoggedIn: true,
-    eventId: eventId
-  };
 });
 
 var checkRequireField = function(params) {
